f4ad738beb1d9e33c3fef0ef42b0851dff8ee570
SandyPham823 <sandydupham@gmail.com>
2019-11-14 13:18:57 -0500

    made variables

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,13 +1,34 @@
-import static java.lang.Character.isLowerCase;
-import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
 
+    private final String upperCaseStart = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String lowerCaseStart = "abcdefghijklmnopqrstuvwxyz";
+
+    protected String startUpper;
+    protected String startLower;
+    protected String registerUpper;
+    protected String registerLower;
+    private boolean symmetric = false;
+
+
     ROT13(Character cs, Character cf) {
+        if (toLowerCase(cs) == 'a' && toLowerCase(cf) == 'n') symmetric = true;
+
+        startUpper = ROT13.rotate(upperCaseStart, Character.toUpperCase(cs));
+        startLower = ROT13.rotate(lowerCaseStart, Character.toLowerCase(cs));
+
+        registerUpper = ROT13.rotate(upperCaseStart, Character.toUpperCase(cf));
+        registerLower = ROT13.rotate(lowerCaseStart, Character.toLowerCase(cf));
+
+        System.out.println(startUpper);
+        System.out.println(registerUpper);
+        System.out.println(startLower);
+        System.out.println(registerLower);
     }
 
     ROT13() {
+        this('a', 'm');
     }
 
 
@@ -27,6 +48,14 @@ public class ROT13  {
     public static String rotate(String s, Character c) {
 
         return "";
+    }
+
+    private String substituteIn(String text, String startUpper, String registerUpper, String startLower, String registerlower){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++){
+            Character ch = text.charAt(i);
+        }
+        return null;
     }
 
 }
