1354c29a3b72f522dfcbde974b959955b0991da8
SandyPham823 <sandydupham@gmail.com>
2019-11-15 15:26:10 -0500

    finished Caesar class, working on the test

--- Crypto/src/Main/Main/Caesar.java
+++ Crypto/src/Main/Main/Caesar.java
@@ -9,8 +9,18 @@ public class Caesar extends ROT13 {
         this('a', 'm');
     }
 
-    public String cipher(String text, int shift){
+    public static StringBuffer cipher(String text, int shift) {
+        StringBuffer result = new StringBuffer();
 
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) {
+                char ch = (char) (((int) text.charAt(i) + shift - 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) text.charAt(i) + shift - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
     }
-
 }
