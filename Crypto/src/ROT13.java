49f0981bc0f5d9a50c05d90ef12d9a0e490f5082
SandyPham823 <sandydupham@gmail.com>
2019-11-15 13:13:19 -0500

    finished rotation method except not when rotating on a specific letter

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -33,21 +33,66 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            if (c >= 'a' && c <= 'm') c += 13;
+            else if (c >= 'A' && c <= 'M') c += 13;
+            else if (c >= 'n' && c <= 'z') c -= 13;
+            else if ( c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            if (c >= 'a' && c <= 'm') c += 13;
+            else if (c >= 'A' && c <= 'M') c += 13;
+            else if (c >= 'n' && c <= 'z') c -= 13;
+            else if ( c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
     public String decrypt(String text) {
-        return text;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            if (c >= 'a' && c <= 'm') c += 13;
+            else if (c >= 'A' && c <= 'M') c += 13;
+            else if (c >= 'n' && c <= 'z') c -= 13;
+            else if ( c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
     public static String rotate(String s, Character c) {
+        for (int i = 0; i < s.length(); i++){
+            StringBuilder sb = new StringBuilder();
+            int j = i;
+            int k = 0;
 
-        return "";
+            for (int k2 = j; k2 < s.length(); k2++) {
+                sb.insert(k, s.charAt(j));
+                k++;
+                j++;
+            }
+
+            j = 0;
+            while (j < i) {
+                sb.insert(k, s.charAt(j));
+                j++;
+                k++;
+                sb.append(c);
+            }
+            return sb.toString();
+        }
+        return null;
     }
 
     private String substituteIn(String text, String startUpper, String registerUpper, String startLower, String registerlower){
